44e0c3798d3070fdbd507f60ddef3471b35ad458
xfeep <easyjava@163.com>
2014-01-05 18:24:12 +0800

    add tests for multipart-formdata & static files

--- test/clojure/nginx/clojure/test_all.clj
+++ test/clojure/nginx/clojure/test_all.clj
@@ -5,14 +5,18 @@
 
 (def ^:dynamic *host* "localhost")
 (def ^:dynamic *port* "8080")
+(def ^:dynamic *debug* false)
 
+(defn debugprintln [& args]
+  (when (true? *debug*)
+      (apply println args)))
 
 (deftest test-naive-simple
   (testing "hello clojure"
            (let [r (client/get (str "http://" *host* ":" *port* "/clojure"))
                  h (:headers r)]
-             (println r)
-             (println "=================hello clojure end=============================")
+             (debugprintln r)
+             (debugprintln "=================hello clojure end=============================")
              (is (= 200 (:status r)))
              (is (= "Hello Clojure & Nginx!" (:body r)))
              (is (= "text/plain" (h "content-type")))
@@ -24,8 +28,8 @@
            (let [r (client/get (str "http://" *host* ":" *port* "/headers"))
                  h (:headers r)
                  b (-> r :body (edn/read-string))]
-             (println r)
-             (println "===============simple headers end =============================")
+             (debugprintln r)
+             (debugprintln "===============simple headers end =============================")
              (is (= 200 (:status r)))
              (is (= "e29b7ffb8a5325de60aed2d46a9d150b" (h "etag")))
              (is (= ["no-store" "no-cache"] (h "cache-control")))
@@ -38,8 +42,8 @@
            (let [r (client/get (str "http://" *host* ":" *port* "/headers") {:headers {"my-header" "mytest"}, :cookies {"tc1" {:value "tc1value"}, "tc2" {:value "tc2value"} } })
                  h (:headers r)
                  b (-> r :body (edn/read-string))]
-             (println r)
-             (println "===============cookie & user defined headers end=============================")
+             (debugprintln r)
+             (debugprintln "===============cookie & user defined headers end=============================")
              (is (= 200 (:status r)))
              (is (= "e29b7ffb8a5325de60aed2d46a9d150b" (h "etag")))
              (is (= ["no-store" "no-cache"] (h "cache-control")))
@@ -54,8 +58,8 @@
            (let [r (client/get (str "http://" *host* ":" *port* "/headers?my=test") {:headers {"my-header" "mytest" "Content-Type" "text/plain; charset=utf-8"}, :cookies {"tc1" {:value "tc1value"}, "tc2" {:value "tc2value"} } })
                  h (:headers r)
                  b (-> r :body (edn/read-string))]
-             (println r)
-             (println "===============query string & character-encoding =============================")
+             (debugprintln r)
+             (debugprintln "===============query string & character-encoding =============================")
              (is (= 200 (:status r)))
              (is (= "e29b7ffb8a5325de60aed2d46a9d150b" (h "etag")))
              (is (= ["no-store" "no-cache"] (h "cache-control")))
@@ -73,8 +77,8 @@
            (let [r (client/get (str "http://" *host* ":" *port* "/form") {:query-params {:foo "bar"}})
                  h (:headers r)
                  b (-> r :body (edn/read-string))]
-             (println r)
-             (println "=================form method=get end=============================")
+             (debugprintln r)
+             (debugprintln "=================form method=get end=============================")
              (is (= 200 (:status r)))
              (is (= "foo=bar" (b :query-string)))
              (is (nil? (b :form-body-str)))
@@ -83,10 +87,60 @@
            (let [r (client/post (str "http://" *host* ":" *port* "/form") {:form-params {:foo "bar"}})
                  h (:headers r)
                  b (-> r :body (edn/read-string))]
-             (println r)
-             (println "=================form method=post end=============================")
+             (debugprintln r)
+             (debugprintln "=================form method=post end=============================")
              (is (= 200 (:status r)))
              (is (= "foo=bar" (b :form-body-str)))
              (is (= "nginx.clojure.NativeInputStream" (b :form-body-type)))
              (is (nil? (b :query-string)))
-             )))
+             ))
+  (testing "form multipart-formdata"
+           (let [r (client/post (str "http://" *host* ":" *port* "/echoUploadfile") {:multipart [{:name "mytoken", :content "123456"},
+                                                                                                 {:name "myf", :content (clojure.java.io/file "test/nginx-working-dir/post-test-data")}
+                                                                                                 ]})
+                 h (:headers r)
+                 b (r :body)]
+             (debugprintln r)
+             (debugprintln "=================form multipart-formdata=============================")
+             (is (= 200 (:status r)))
+             (is (< 0 (.indexOf b "name=\"mytoken\"")))
+             (is (< 0 (.indexOf b "123456")))
+             (is (< 0 (.indexOf b "name=\"myf\"")))
+             (is (< 0 (.indexOf b "Apache HTTP Server Version 2.4")))
+             (is (< 0 (.indexOf b "Modules | Directives | FAQ | Glossary | Sitemap")))
+             ))
+  )
+
+
+(deftest test-file
+  (testing "static file without gzip"
+           (let [r (client/get (str "http://" *host* ":" *port* "/testfiles/small.html") {:decompress-body false})
+                 h (:headers r)
+                 b (r :body)]
+             (debugprintln r)
+             (debugprintln "=================static file (no gzip) end =============================")
+             (is (= 200 (:status r)))
+             (is (= "680" (h "content-length")))))
+    (testing "static file with gzip"
+           ;clj-http will auto use Accept-Encoding	gzip, deflate
+           (let [r (client/get (str "http://" *host* ":" *port* "/testfiles/small.html"))
+                 h (:headers r)
+                 b (r :body)]
+             (debugprintln r)
+             (debugprintln "=================static file (with gzip) end=============================")
+             (is (= 200 (:status r)))
+             (is (= "gzip" (:orig-content-encoding r)))
+             (is (= 680 (count (r :body))))))
+    
+   (testing "static file with range operation"
+      ;clj-http will auto use Accept-Encoding	gzip, deflate
+      (let [r (client/get (str "http://" *host* ":" *port* "/testfiles/small.html")  {:decompress-body false, :headers {"Range" "bytes=0-128"}})
+            h (:headers r)
+            b (r :body)]
+        (debugprintln r)
+        (debugprintln "=================static file (with range 0-128) end=============================")
+        ;206 Partial Content
+        (is (= 206 (:status r)))
+        (is (= 129 (count (r :body))))))
+    
+  )
