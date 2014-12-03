2b70166f87984fd290429a20ab15bf817db90f3a
xfeep <xfeepz@gmail.com>
2014-12-04 00:35:17 +0800

    filter request interface

---
+++ src/java/nginx/clojure/java/NginxJavaFilterRequest.java
@@ -0,0 +1,49 @@
+package nginx.clojure.java;
+
+import static nginx.clojure.MiniConstants.NGX_HTTP_CLOJURE_HEADERSO_STATUS_OFFSET;
+import static nginx.clojure.MiniConstants.NGX_HTTP_CLOJURE_REQ_HEADERS_OUT_OFFSET;
+import static nginx.clojure.NginxClojureRT.fetchNGXInt;
+import static nginx.clojure.NginxClojureRT.pushNGXInt;
+
+import java.util.Map;
+
+import nginx.clojure.NginxFilterRequest;
+import nginx.clojure.NginxHandler;
+
+public class NginxJavaFilterRequest extends NginxJavaRequest implements NginxFilterRequest {
+
+	/**
+	 * native ngx_chain_t
+	 */
+	protected long c;
+	
+	/**
+	 * native headers_out
+	 */
+	protected long ho;
+	
+	protected Map<String, Object> responseHeaders;
+	
+	public NginxJavaFilterRequest(NginxHandler handler, NginxJavaRingHandler ringHandler, long r, long c)  {
+		super(handler, ringHandler, r);
+//		long pool = NginxClojureRT.UNSAFE.getAddress(r + NGX_HTTP_CLOJURE_REQ_POOL_OFFSET);
+		ho = r + NGX_HTTP_CLOJURE_REQ_HEADERS_OUT_OFFSET;
+		responseHeaders = new JavaLazyHeaderMap(r, true);
+	}
+	
+	@Override
+	public int responseStatus() {
+		return fetchNGXInt(ho + NGX_HTTP_CLOJURE_HEADERSO_STATUS_OFFSET);
+	}
+
+	public NginxJavaFilterRequest responseStatus(int status) {
+		pushNGXInt(ho + NGX_HTTP_CLOJURE_HEADERSO_STATUS_OFFSET, status );
+		return this;
+	}
+	
+	@Override
+	public Map<String, Object> responseHeaders() {
+		return responseHeaders;
+	}
+
+}
