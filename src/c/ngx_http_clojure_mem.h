8ff75339c51dcaad53f2fbc51c55c8006448bd6c
xfeep <xfeepz@gmail.com>
2015-08-23 22:08:09 +0800

    change ver to 0.4.2

--- src/c/ngx_http_clojure_mem.h
+++ src/c/ngx_http_clojure_mem.h
@@ -29,12 +29,12 @@
 #include <inttypes.h>
 #endif
 
-#define nginx_clojure_ver  4001 /*0.4.0*/
+#define nginx_clojure_ver  4002 /*0.4.0*/
 
 /*the least jar version required*/
-#define nginx_clojure_required_rt_lver 4001
+#define nginx_clojure_required_rt_lver 4002
 
-#define NGINX_CLOJURE_VER_NUM_STR "0.4.1"
+#define NGINX_CLOJURE_VER_NUM_STR "0.4.2"
 
 #define NGINX_CLOJURE_VER "nginx-clojure/" NGINX_CLOJURE_VER_NUM_STR
 
