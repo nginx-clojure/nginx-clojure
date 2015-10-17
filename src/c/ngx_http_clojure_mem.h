67e449838fe1bba4312e1e96068d6e6b515fa9ae
xfeep <xfeepz@gmail.com>
2015-10-18 01:31:23 +0800

    #if windows, fix typo error: uint864_t -> uint64_t

--- src/c/ngx_http_clojure_mem.h
+++ src/c/ngx_http_clojure_mem.h
@@ -29,7 +29,7 @@
 
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int8 uint8_t;
-typedef unsigned __int64 uint864_t;
+typedef unsigned __int64 uint64_t;
 
 #define JVM_CP_SEP ';'
 #define JVM_CP_SEP_S ";"
