33b0d9fb06eadba3f0263a09522658e2149839d9
xfeep <xfeepz@gmail.com>
2017-08-10 23:23:16 +0800

    init jersey example

---
+++ example-projects/jersey-example/src/main/java/nginx/clojure/jersey/example/MyResource.java
@@ -0,0 +1,32 @@
+package nginx.clojure.jersey.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    @GET
+    @Path("/empty")
+    public Response empty() {
+    	return Response.status(201).entity("").build();
+    }
+}
