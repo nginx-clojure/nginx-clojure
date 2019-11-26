a56169cb4ec949451e2db8d2238c23c27fc11ef9
xfeep <xfeep@outlook.com>
2019-11-26 13:31:38 +0800

    :pencil: Fix readme & spring framework example pom.xml

--- example-projects/spring-core-example/src/main/java/nginx/clojure/spring/core/example/NginxSpringRewriteHandler.java
+++ example-projects/spring-core-example/src/main/java/nginx/clojure/spring/core/example/NginxSpringRewriteHandler.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import nginx.clojure.MiniConstants;
-import nginx.clojure.java.ArrayMap;
 import nginx.clojure.java.Constants;
 import nginx.clojure.java.NginxJavaRequest;
 import nginx.clojure.java.NginxJavaRingHandler;
