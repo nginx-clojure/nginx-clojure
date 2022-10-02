0b9d2a71ff3d323ef041707c899f8e6badc02e51
xfeep <xfeep@outlook.com>
2022-10-02 21:23:09 +0800

    Fix version #272

--- project.clj
+++ project.clj
@@ -73,10 +73,10 @@
                                   [org.clojure/tools.trace "0.7.10"]
                                   ]}
              :unittest {
-                    :jvm-opts ["-javaagent:target/nginx-clojure-0.5.3.jar=mb"
+                    :jvm-opts ["-javaagent:target/nginx-clojure-0.6.0.jar=mb"
                                "-Dfile.encoding=UTF-8"
                                "-Dnginx.clojure.wave.udfs=pure-clj.txt,compojure.txt,compojure-http-clj.txt,mysql-jdbc.txt,test-groovy.txt"
-                               "-Xbootclasspath/a:target/nginx-clojure-0.5.3.jar"]
+                               "-Xbootclasspath/a:target/nginx-clojure-0.6.0.jar"]
                     :junit-options {:fork "on"}
                     :java-source-paths ["test/java" "test/clojure"]
                     :test-paths ["src/test/clojure"]
